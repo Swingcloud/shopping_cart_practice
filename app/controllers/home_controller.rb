d562a1c9aa8f8d4cb202206426042f1ce8cf1c3c
Sam Lin <greenlin111@gmail.com>
2016-12-07 16:30:45 +0800

    feat(Routes): Use Nested Resources on category and product

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
 	def index
-
+		@categories = Category.where(enabled: true).all
+		@products = Product.order('id desc').page(params[:page])
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resources :categories, only: :show do 
+  	resources :products, controller: :category_products, only: :show
+  end
 	root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
