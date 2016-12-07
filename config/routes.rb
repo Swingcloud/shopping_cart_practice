11dae067a702078ae58261a4e8db45be86fa47c1
Sam Lin <greenlin111@gmail.com>
2016-12-07 16:37:17 +0800

    feat(Routes): Ruled via Http Verb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :categories, only: :show do 
   	resources :products, controller: :category_products, only: :show
   end
+
+  get '/about' => 'pages#about'
+  get '/contact' => 'pages#contact'
 	root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
