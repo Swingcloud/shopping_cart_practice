837a1fe7a19bec89c15bf3dd15da9af547a17cc2
Sam Lin <greenlin111@gmail.com>
2016-12-08 15:40:18 +0800

    feat(Admin): Add admin layout

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,10 +2,12 @@ class Admin::BaseController < ApplicationController
 	before_action :authenticate_user!
 	before_action :authenticate_admin!
 
+	layout 'admin'
+	
 	private
 
 	def authenticate_admin!
 		redirect_to rrot_path, notice: 'you are not admin' unless current_user.admin?
 	end
-	
+
 end
