757e54499dd5541cf0ec3e72279dc3d0164019c1
Sam Lin <greenlin111@gmail.com>
2016-12-09 18:31:13 +0800

    feat(Model): Change definition of Product.active

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 	belongs_to :category
 	validates_presence_of :name, :price
 
-	scope :active, ->{ where(active: true )}
+	scope :active, ->{ where(active: true).where('shelved_on <= ?', Date.current)}
 
 	def to_param
 		"#{id}-#{friendly_id}"
