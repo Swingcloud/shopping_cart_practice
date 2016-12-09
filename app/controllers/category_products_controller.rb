e60f5c542cbca64d04ac0ab9e5ec9b70645dbff8
Sam Lin <greenlin111@gmail.com>
2016-12-09 16:18:10 +0800

    feat(Scope): Replace query with scope in controllers

--- app/controllers/category_products_controller.rb
+++ app/controllers/category_products_controller.rb
@@ -2,7 +2,7 @@ class CategoryProductsController < ApplicationController
 	before_action :find_category
 
 	def show
-		@product = @category.products.where(active: true).find(params[:id].to_i)
+		@product = @category.products.active.find(params[:id].to_i)
 		viewed_at = cookies["view-product-#{@product.id}"].to_i
 		puts viewed_at
 		@viewed = viewed_at > @product.updated_at.to_i
@@ -14,6 +14,6 @@ class CategoryProductsController < ApplicationController
 	# Check it in`rails routes`
   # category_product GET /categories/:category_id/products/:id(.:format) category_products#show
  	def find_category
- 		@category = Category.where(enabled: true).find(params[:category_id])
+ 		@category = Category.enabled.find(params[:category_id])
  	end
 end
