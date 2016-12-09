241cd5632b1f535bb43a0484547996b38e36b7c6
Sam Lin <greenlin111@gmail.com>
2016-12-09 16:13:43 +0800

    feat(Model): Add scope to category & product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
 	belongs_to :category
 	validates_presence_of :name, :price
 
+	scope :active, ->{ where(active: true )}
+
 	def to_param
 		"#{id}-#{friendly_id}"
 	end
