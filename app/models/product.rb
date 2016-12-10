ab34736bac0b069542947acab8a27637d9e4f39b
Sam Lin <greenlin111@gmail.com>
2016-12-10 16:35:25 +0800

    feat(Model): Add model of Cart, CartItem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
 	belongs_to :category
+	has_many :cart_items, foreign_key: 'item_id', dependent: :destroy
+	has_many :carts, through: :cart_items
+
 	validates_presence_of :name, :price
 
 	scope :active, ->{ where(active: true).where('shelved_on <= ?', Date.current)}
