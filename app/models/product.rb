3bff8cdee965a5637a4d572d8ba31e90a91a28d8
Sam Lin <greenlin111@gmail.com>
2016-12-26 09:00:43 +0800

    feat(Model): Add model of Comment

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
 	has_many :cart_items, foreign_key: 'item_id', dependent: :destroy
 	has_many :carts, through: :cart_items
 	has_many :order_items, dependent: :destroy
-
+	has_many :comments, as: :commentable
 	validates_presence_of :name, :price
 
 	scope :active, ->{ where(active: true).where('shelved_on <= ?', Date.current)}
